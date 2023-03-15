e5945b75ea47b1afa1f4c8f40f732e6ec3b94267
Breno Queiroz <brenoqueirozvds@gmail.com>
2023-03-15 07:44:01 -0300

    ECS-611 Adding @Autowired annotation

--- src/test/java/org/entando/entando/plugins/jpcontentscheduler/aps/system/services/content/TestContentScheduler.java
+++ src/test/java/org/entando/entando/plugins/jpcontentscheduler/aps/system/services/content/TestContentScheduler.java
@@ -6,11 +6,13 @@ import org.entando.entando.plugins.jpcontentscheduler.aps.system.services.conten
 
 import com.agiletec.aps.system.exception.ApsSystemException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestContentScheduler extends ApsPluginBaseTestCase {
 
+	@Autowired
 	private IContentSchedulerManager contentScheduler;
 
 	@Test
@@ -20,11 +22,6 @@ public class TestContentScheduler extends ApsPluginBaseTestCase {
 		ContentThreadConfigDOM dd = new ContentThreadConfigDOM();
 		String xml = dd.createConfigXml(config);
 		System.out.println(xml);
-	}
-
-	public void init() throws Exception {
-		this.contentScheduler = (IContentSchedulerManager) this.getApplicationContext().getBean("jpcontentschedulerContentSchedulerManager");
-		super.setUp();
 	}
 
 }
